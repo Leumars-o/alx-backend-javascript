6ad47fba166c328e51441b99e519ae8e6ba2c4f9
leumars-o <samex150@gmail.com>
2024-08-08 02:52:13 +0100

    handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((values) => (
+      values.map((value) => ({
+        status: value.status,
+        data: value.status === 'fulfiled' ? value.data : String(value.reason),
+      }))
+    ));
+}
