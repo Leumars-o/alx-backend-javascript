47443ba8d374dcb12af5b2d6f8a160bb4cc07b05
leumars-o <samex150@gmail.com>
2024-08-10 00:53:53 +0100

    final user imported functions modified

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,3 @@
 export default function signUpUser(firstName, lastName) {
-  return new Promise((resolve, reject) => {
-    if (firstName && lastName) {
-      resolve({ firstName, lastName });
-    } else {
-      reject(new Error('Invalid input'));
-    }
-  });
+  return Promise.resolve({ firstName, lastName });
 }
