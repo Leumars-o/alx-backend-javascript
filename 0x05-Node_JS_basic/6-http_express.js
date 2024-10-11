75820ddd84e9c2807cad4ad88f615a2021a44576
leumars-o <samex150@gmail.com>
2024-10-11 04:38:03 +0100

    script modified to task requirements

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 1235;
 
 app.get('/', (_, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+module.exports = app;
